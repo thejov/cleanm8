22ac880a9d8323152f999ba66cf22643a5045673
Juha Viljanen <juha.o.viljanen@gmail.com>
2016-03-03 23:17:36 +0200

    User has to login to access any page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,15 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
+  before_filter :require_login
+
   private
+
+  def require_login
+    unless current_user
+      redirect_to log_in_path
+    end
+  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]

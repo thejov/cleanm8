aac50765b5d3a4e3d4162efeea973675ba036b6e
Juha Viljanen <juha.o.viljanen@gmail.com>
2016-03-08 21:55:23 +0200

    Validation for user name given

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   validates_presence_of :password, :on => :create
   validates_presence_of :email
   validates_uniqueness_of :email
+  validates_presence_of :name
 
   def self.authenticate(email, password)
     user = find_by_email(email)

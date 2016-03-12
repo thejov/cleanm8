fa1ea12301851899473ef6900b300342b78f55aa
Juha Viljanen <juha.o.viljanen@gmail.com>
2016-03-12 12:09:41 +0200

    Add tests for chore model

--- test/models/chore_test.rb
+++ test/models/chore_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ChoreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'saving chore with name should succeed' do
+    a_chore = Chore.new(:name => 'foo')
+    assert a_chore.save, 'Saving chore with valid name failed!'
+  end
+
+  test 'should not save chore without name' do
+    a_chore = Chore.new()
+    assert_not a_chore.save, 'Saved chore without a name!'
+  end
+
 end

b319f95299bb72de6c7a99db02904c18020e8db2
Juha Viljanen <juha.o.viljanen@gmail.com>
2016-03-19 00:13:10 +0200

    Validate presence of date, chore_id and user_id in cleaning

--- app/models/cleaning.rb
+++ app/models/cleaning.rb
@@ -1,4 +1,8 @@
 class Cleaning < ApplicationRecord
   belongs_to :chore
   belongs_to :user
+
+  validates_presence_of :date
+  validates_presence_of :chore_id
+  validates_presence_of :user_id
 end

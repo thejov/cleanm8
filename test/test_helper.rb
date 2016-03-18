780d510b0fedeb242b81c11a82a85f5470215a3e
Juha Viljanen <juha.o.viljanen@gmail.com>
2016-03-19 00:10:27 +0200

    Move TestUserBuilder class to test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,54 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def create_user
+    TestUserBuilder.new
+  end
+
+end
+
+class TestUserBuilder
+  def initialize
+    @name = 'John Doe'
+    @email = 'foo@example.com'
+    @password = 'secret'
+  end
+
+  def with_name(name)
+    @name = name
+    self
+  end
+
+  def with_email(email)
+    @email = email
+    self
+  end
+
+  def with_password(password)
+    @password = password
+    self
+  end
+
+  def without_name
+    @name = nil
+    self
+  end
+
+  def without_email
+    @email = nil
+    self
+  end
+
+  def without_password
+    @password = nil
+    self
+  end
+
+  def build
+      user_attributes = {}
+      user_attributes[:name] = @name if @name
+      user_attributes[:email] = @email if @email
+      user_attributes[:password] = @password if @password
+      User.new( user_attributes )
+  end
 end

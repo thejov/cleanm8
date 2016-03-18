91fa30fd5f33c335b4b8b10a2d16803e18874b36
Juha Viljanen <juha.o.viljanen@gmail.com>
2016-03-19 00:13:50 +0200

    Add tests for cleaning model

--- test/models/cleaning_test.rb
+++ test/models/cleaning_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class CleaningTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+
+  end
+
+  test 'saving cleaning with date chore and user should succeed' do
+    a_cleaning = create_cleaning.build
+
+    assert a_cleaning.save, 'Saving cleaning for a valid chore and user with valid date failed!'
+  end
+
+  test 'should not save cleaning without arguments' do
+    a_cleaning = Cleaning.new()
+    assert_not a_cleaning.save, 'Should not have saved cleaning without any given arguments!'
+  end
+
+  test 'should not save cleaning without a date' do
+    a_cleaning = create_cleaning.without_date.build
+    assert_not a_cleaning.save, 'Should not have saved cleaning without a date given!'
+  end
+
+  test 'should not save cleaning without a chore' do
+    a_cleaning = create_cleaning.without_chore.build
+    assert_not a_cleaning.save, 'Should not have saved cleaning without a chore given!'
+  end
+
+  test 'should not save cleaning without a user' do
+    a_cleaning = create_cleaning.without_user.build
+    assert_not a_cleaning.save, 'Should not have saved cleaning without a user given!'
+  end
+
+  test 'should not save cleaning with nonexistent chore id' do
+    assert_not Chore.find_by_id(777), 'Chore with id 777 should not exist!'
+    a_cleaning = create_cleaning.with_chore(777).build
+    assert_not a_cleaning.save, 'Should not have saved cleaning with nonexistent chore id!'
+  end
+
+  test 'should not save cleaning with nonexistent user id' do
+    assert_not User.find_by_id(777), 'User with id 777 should not exist!'
+    a_cleaning = create_cleaning.with_user(777).build
+    assert_not a_cleaning.save, 'Should not have saved cleaning with nonexistent user id!'
+  end
+
+  private
+
+  def create_cleaning
+    TestCleaningBuilder.new
+  end
+
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -55,3 +55,53 @@ class TestUserBuilder
       User.new( user_attributes )
   end
 end
+
+class TestCleaningBuilder
+
+  require 'test_helper'
+
+  def initialize
+    @date = DateTime.now
+
+    a_chore = Chore.new(:name => 'foo')
+    a_chore.save
+    @chore_id = a_chore.id
+
+    a_user = TestUserBuilder.new.build
+    a_user.save
+    @user_id = a_user.id
+  end
+
+  def without_date
+    @date = nil
+    self
+  end
+
+  def without_chore
+    @chore_id = nil
+    self
+  end
+
+  def without_user
+    @user_id = nil
+    self
+  end
+
+  def with_chore(id)
+    @chore_id = id
+    self
+  end
+
+  def with_user(id)
+    @user_id = id
+    self
+  end
+
+  def build
+    user_attributes = {}
+    user_attributes[:date] = @date if @date
+    user_attributes[:chore_id] = @chore_id if @chore_id
+    user_attributes[:user_id] = @user_id if @user_id
+    Cleaning.new( user_attributes )
+  end
+end

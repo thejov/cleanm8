135b2d4a9554f5cc8025f7006abdfb2602e4bca9
Juha Viljanen <juha.o.viljanen@gmail.com>
2016-03-19 17:29:32 +0200

    Add tests for user controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,29 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get users_new_url
+class UsersControllerTest < ActionController::TestCase
+  test 'should get new' do
+    get :new
     assert_response :success
   end
+
+  test 'should create user' do
+    assert_difference('User.count') do
+      create_user
+    end
+  end
+
+  test 'should not accept undefined parameters' do
+    assert_no_difference('User.count') do
+      post :create, user: { foo: 'bar' }
+    end
+  end
+
+  private
+
+  def create_user
+    post :create, user: { name: 'John Doe', email: 'foo@example.com',
+                          password: 'secret', password_confirmation: 'secret' }
+  end
+
 
 end

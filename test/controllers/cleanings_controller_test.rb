dcb231ed75cc43e3490657ae3e93c20836ddf126
Juha Viljanen <juha.o.viljanen@gmail.com>
2016-03-25 13:10:40 +0200

    Add tests for cleaning controller

--- test/controllers/cleanings_controller_test.rb
+++ test/controllers/cleanings_controller_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class CleaningsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    signup_and_login
+    @chore = chores(:one)
+  end
+
+  test 'should create cleaning' do
+    assert_difference('Cleaning.count') do
+      create_cleaning
+    end
+  end
+
+  test 'should show notice about created cleaning' do
+    create_cleaning
+    assert_equal 'Cleaning was added successfully.', flash.notice
+  end
+
+  test 'should not create cleaning without logged in user' do
+    logout
+    assert_no_difference('Cleaning.count') do
+      create_cleaning
+    end
+  end
+
+  test 'should raise exception when creating cleaning with non-existent chore id' do
+    assert_raise ActiveRecord::RecordNotFound do
+      post cleanings_url, params: { cleaning: { chore_id: 777 } }
+    end
+  end
+
+  private
+
+  def create_cleaning
+    post cleanings_url, params: { cleaning: { chore_id: @chore.id } }
+  end
+
 end
